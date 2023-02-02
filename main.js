8ed11ddcf1e1ce4e04ca71b22aae0f6740efdfe8
1443501 <106714590+1443501@users.noreply.github.com>
2023-02-01 19:36:45 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -5,11 +5,13 @@ rover_height=90;
 roverx=10;
 rovery=10;
 rover_image="rover.png";
-mars_image="mars.jpg";
+mars_image=["mars.jpg","Mars1.jfif","mars2.jpg","mars3.jpg","mars4.jfif"];
+randomnum=Math.floor(Math.random()*5);
+backgroundImage=mars_image[randomnum];
 function add(){
     background_imgTag= new Image();
     background_imgTag.onload= uploadBackground;
-    background_imgTag.src=mars_image;
+    background_imgTag.src=backgroundImage;
     rover_imgTag= new Image();
     rover_imgTag.onload= uploadrover;
     rover_imgTag.src=rover_image;
@@ -40,5 +42,36 @@ left();
 right();
 
     }
+
     
 }
+function up(){
+    if(rovery>=0){
+        rovery=rovery-10;
+        uploadBackground();
+        uploadrover();
+    }
+}
+function down(){
+    if(rovery<=500){
+        rovery=rovery+10;
+        uploadBackground();
+        uploadrover();
+    }
+}
+function left(){
+    if(roverx>=0){
+        roverx=roverx-10;
+        uploadBackground();
+        uploadrover();
+    }
+}
+function right(){
+    if(roverx<=700){
+        roverx=roverx+10;
+        uploadBackground();
+        uploadrover();
+    }
+}
+
+
